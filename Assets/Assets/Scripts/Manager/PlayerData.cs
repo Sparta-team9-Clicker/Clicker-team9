b52582b47e9c6ac811b6f4e462a7535f737be4ce
Jujeongmin <anjshdkdl99@gmail.com>
2025-03-27 16:27:18 +0900

    [Feat] 스탯 및 재화 구현

---
+++ Assets/Assets/Scripts/Manager/PlayerData.cs
@@ -0,0 +1,28 @@
+[System.Serializable]
+public class PlayerData
+{
+    public int stage;
+    public int attackPower;
+    public int criticalDamage;
+    public float critical;
+    public int gold;
+    public int goldBonusUpgrade;
+    public int attackUpgrade;
+    public int criticalUpgrade;
+    public int criticalDamageUpgrade;
+    public bool Eqiup;
+
+    public PlayerData(int stage, int attackPower, int criticalDamage, float critical, int gold, int goldBonusUpgrade,int attackUpgrade, int criticalUpgrade, int criticalDamageUpgrade, bool eqiup)
+    {
+        this.stage = stage;
+        this.attackPower = attackPower;
+        this.criticalDamage = criticalDamage;
+        this.critical = critical;
+        this.gold = gold;
+        this.goldBonusUpgrade = goldBonusUpgrade;
+        this.attackUpgrade = attackUpgrade;
+        this.criticalUpgrade = criticalUpgrade;
+        this.criticalDamageUpgrade = criticalDamageUpgrade;
+        Eqiup = eqiup;
+    }
+}
