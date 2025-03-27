ee4ea644d80def6490d4cb18a089302980e7da18
hee103 <gmlgml3213@gmail.com>
2025-03-27 17:42:29 +0900

    [Feat] 몬스터 프리팹, 애니메이셤 추가

--- Assets/ScriptableObject_JHW/MonsterData.cs
+++ Assets/ScriptableObject_JHW/MonsterData.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class MonsterData : ScriptableObject
 {
     [Header("Info")]
-    [SerializeField] private string monsterName;
-    [SerializeField] private float monsterHP;
-    [SerializeField] private GameObject monsterPrefab;
+    public string monsterName;
+    public float monsterHP;
+    public GameObject monsterPrefab;
 }
