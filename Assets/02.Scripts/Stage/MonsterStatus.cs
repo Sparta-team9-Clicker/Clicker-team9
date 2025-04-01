9532ad0d93c9ad692856cff59f125c6cd7cc2d7a
hee103 <gmlgml3213@gmail.com>
2025-04-01 21:01:18 +0900

    [Docs]  스트립트 주석 추가

--- Assets/02.Scripts/Stage/MonsterStatus.cs
+++ Assets/02.Scripts/Stage/MonsterStatus.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 public class MonsterStatus : MonoBehaviour
 {
     public MonsterData monsterData;
-    private int currentHP;
     public MonsterStatusUI monsterStatusUI;
     private Animator animator;
+    private int currentHP;
 
     public void Start()
     {
         animator = GetComponent<Animator>();
     }
 
+    // UI 업데이트를 위해 MonsterStatusUI에 데이터를 전달
     public void SetMonsterData(MonsterData data)
     {
         monsterData = data;
@@ -29,16 +30,21 @@ public class MonsterStatus : MonoBehaviour
         }
     }
 
+    // 몬스터가 피해를 입는 함수
     public void TakeDamage(int damage)
     {
         animator.SetTrigger("isDamage");
+
+        // 체력 감소 
         currentHP = Mathf.Max(currentHP - damage, 0);
 
+        // UI 업데이트
         if (monsterStatusUI != null)
         {
             monsterStatusUI.UpdateHP(currentHP);
         }
 
+        // 체력이 0 이하이면 사망 처리
         if (currentHP <= 0)
         {
             Die();
@@ -48,7 +54,11 @@ public class MonsterStatus : MonoBehaviour
     private void Die()
     {
         StageManager.Instance.OnMonsterKilled();
+
+        // 처치보상 골드 지급
         GameManager.Instance.playerData.gold += monsterData.rewardGold;
+        
+        // 오브젝트 삭제
         Destroy(gameObject);
     }
 }
--- Assets/02.Scripts/Stage/MonsterStatusUI.cs
+++ Assets/02.Scripts/Stage/MonsterStatusUI.cs
@@ -4,27 +4,32 @@ using UnityEngine.UI;
 
 public class MonsterStatusUI : MonoBehaviour
 {
-    private int curValue;
-    private int maxValue;
-    public TextMeshProUGUI monsterName;
-    public Slider uiBar;
+    private int curValue; // 현재 몬스터 체력
+    private int maxValue; // 몬스터 최대 체력
+    public TextMeshProUGUI monsterName; // 몬스터 이름 표시 UI
+    public Slider uiBar; // 몬스터 체력을 표시하는 UI
 
+    //UI 요소(체력 바, 몬스터 이름)를 초기화하는 역할
     public void SetMonsterData(MonsterData monsterData)
     {
         if (monsterData != null)
         {
             maxValue = monsterData.monsterHP;
             curValue = maxValue;
+
+            // UI 바의 최대값과 현재 체력 동기화
             uiBar.maxValue = maxValue;
             uiBar.value = curValue;
+
+            // 몬스터 이름 표시
             monsterName.text = monsterData.monsterName;
-            Debug.Log($"몬스터 체력: {curValue}");
         }
     }
 
+    // 체력 UI를 업데이트하는 함수
     public void UpdateHP(int hp)
     {
-        curValue = hp;
-        uiBar.value = curValue;
+        curValue = hp; // 전달받은 hp 값으로 갱신
+        uiBar.value = curValue; // UI 바의 값을 업데이트하여 체력 표시
     }
 }
