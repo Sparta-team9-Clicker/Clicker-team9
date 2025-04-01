6d400178fca92ebf09480b3df5efa5d008f54ce0
Astorius503 <tiggh12@gmail.com>
2025-04-01 21:04:06 +0900

    Revert "Merge branch 'develope' of https://github.com/Sparta-team9-Clicker/Clicker-team9 into develope"
    This reverts commit d670a4f79d730bc02e82b4a9af2978b2d007f6e7, reversing
    changes made to 68e30436d6a877ba440c26a6b4a487ff4713bf49.

--- Assets/02.Scripts/ScriptableObject/MonsterData.cs
+++ Assets/02.Scripts/ScriptableObject/MonsterData.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-// 스테이지 유형을 정의
 public enum StageType
 {
     Easy,
@@ -11,18 +10,16 @@ public enum StageType
 [CreateAssetMenu(fileName = "MonsterDataTable", menuName = "Data/MonsterTable")]
 public class MonsterDataTable : ScriptableObject
 {
-    // 몬스터 데이터 배열
     public MonsterData[] monsters;
 }
 
-// 몬스터 데이터 정보를 저장할 클래스
 [System.Serializable]
 public class MonsterData
 {
-    public string monsterName; // 몬스터 이름
-    public StageType stageType; // 스테이지 유형
-    public int monsterHP; // 몬스터 체력
-    public int rewardGold; // 처치 보상 골드
-    public GameObject monsterPrefab;  // 몬스터 프리팹
+    public string monsterName;
+    public StageType stageType;
+    public int monsterHP;
+    public int rewardGold;
+    public GameObject monsterPrefab; 
 }
 
--- Assets/02.Scripts/Stage/MonsterStatus.cs
+++ Assets/02.Scripts/Stage/MonsterStatus.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 public class MonsterStatus : MonoBehaviour
 {
     public MonsterData monsterData;
+    private int currentHP;
     public MonsterStatusUI monsterStatusUI;
     private Animator animator;
-    private int currentHP;
 
     public void Start()
     {
         animator = GetComponent<Animator>();
     }
 
-    // UI 업데이트를 위해 MonsterStatusUI에 데이터를 전달
     public void SetMonsterData(MonsterData data)
     {
         monsterData = data;
@@ -30,21 +29,16 @@ public class MonsterStatus : MonoBehaviour
         }
     }
 
-    // 몬스터가 피해를 입는 함수
     public void TakeDamage(int damage)
     {
         animator.SetTrigger("isDamage");
-
-        // 체력 감소 
         currentHP = Mathf.Max(currentHP - damage, 0);
 
-        // UI 업데이트
         if (monsterStatusUI != null)
         {
             monsterStatusUI.UpdateHP(currentHP);
         }
 
-        // 체력이 0 이하이면 사망 처리
         if (currentHP <= 0)
         {
             Die();
@@ -54,11 +48,7 @@ public class MonsterStatus : MonoBehaviour
     private void Die()
     {
         StageManager.Instance.OnMonsterKilled();
-
-        // 처치보상 골드 지급
         GameManager.Instance.playerData.gold += monsterData.rewardGold;
-        
-        // 오브젝트 삭제
         Destroy(gameObject);
     }
 }
